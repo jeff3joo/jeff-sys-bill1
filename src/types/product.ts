94bad0cd01c8b88d69238c860a73e1ea53d8bbe5
Jeffrin Rijo V C <jeffrinrijo4@gmail.com>
2026-08-16 14:46:41 +0530

    feat: add products database and security

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export type ProductType = "product" | "service";
+
+export interface Product {
+  id: string;
+  name: string;
+  type: ProductType;
+  category: string;
+  mrp: number;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+}
